1e976ce958fb9df407b9c2f9d89fbc4c12e4b685
Pepe <jose.martinez.del.rio@outlook.com>
2025-07-01 12:12:04 +0200

    Whole card is clickable - JM

--- app/components/episodeItem/EpisodeCard.jsx
+++ app/components/episodeItem/EpisodeCard.jsx
@@ -3,13 +3,14 @@ import styles from "./EpisodeCard.module.css";
 
 export default function EpisodeCard({ episode }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardMainText}>{episode.episode}</div>
-      <div className={styles.cardText1}>{episode.name}</div>
-      <div className={styles.cardText2}>{`Air Date: ${episode.air_date}`}</div>
-      <Link href={`/episodes/${episode.id}`} className={styles.link}>
-        Ver detalle
-      </Link>
-    </div>
+    <Link href={`/episodes/${episode.id}`}>
+      <div className={styles.card}>
+        <div className={styles.cardMainText}>{episode.episode}</div>
+        <div className={styles.cardText1}>{episode.name}</div>
+        <div
+          className={styles.cardText2}
+        >{`Air Date: ${episode.air_date}`}</div>
+      </div>
+    </Link>
   );
 }
--- app/components/locationItem/LocationCard.jsx
+++ app/components/locationItem/LocationCard.jsx
@@ -3,15 +3,14 @@ import styles from "./LocationCard.module.css";
 
 export default function LocationCard({ location }) {
   return (
-    <div className={styles.card}>
-      <div className={styles.cardMainText}>{location.name}</div>
-      <div className={styles.cardText1}>{`Tipo: ${location.type}`}</div>
-      <div
-        className={styles.cardText2}
-      >{`Dimensión: ${location.dimension}`}</div>
-      <Link href={`/locations/${location.id}`} className={styles.link}>
-        Ver detalle
-      </Link>
-    </div>
+    <Link href={`/locations/${location.id}`}>
+      <div className={styles.card}>
+        <div className={styles.cardMainText}>{location.name}</div>
+        <div className={styles.cardText1}>{`Tipo: ${location.type}`}</div>
+        <div
+          className={styles.cardText2}
+        >{`Dimensión: ${location.dimension}`}</div>
+      </div>
+    </Link>
   );
 }
